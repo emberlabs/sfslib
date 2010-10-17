fc7f51c5f30ddbf9ae36ad4efda7ef3fc4af676b
Obsidian <stratosphere.programming@gmail.com>
2010-10-17 10:25:49 -0500

    update with latest work on SFSResult.  Still a ways to go.

--- src/SFSResult.php
+++ src/SFSResult.php
@@ -6,15 +6,94 @@ class SFSResult /*implements ArrayAccess*/
 
 	protected $raw_data = array();
 
-	public function __construct(SFS $sfs, array $data)
+	protected $successful = false;
+
+	protected $username = array();
+
+	protected $username_data = '';
+
+	protected $username_appears = false;
+
+	protected $username_frequency = 0;
+
+	protected $username_lastseen = '';
+
+	protected $email = array();
+
+	protected $email_data = '';
+
+	protected $email_appears = false;
+
+	protected $email_frequency = 0;
+
+	protected $email_lastseen = '';
+
+	protected $ip = array();
+
+	protected $ip_data = '';
+
+	protected $ip_appears = false;
+
+	protected $ip_frequency = 0;
+
+	protected $ip_lastseen = '';
+
+	public function __construct(SFS $sfs, array $data, array $requested_data)
 	{
+		// Store the main SFS object
 		$this->sfs = $sfs;
 
+		// Store the raw data we received...
 		$this->raw_data = $data;
+
+		$this->successful = ($data['success'] === 1) ? true : false;
+
+		$this->username_data = $requested_data['username'];
+		$this->email_data = $requested_data['email'];
+		$this->ip_data = $requested_data['ip'];
+
+		// at the end of all of this, run these methods...
+		$this->username = $this->getUsernameToArray();
+		$this->email = $this->getEmailToArray();
+		$this->ip = $this->getIPToArray();
 	}
 
+	/**
+	 * Return the array of raw data that was provided to this object
+	 * @return array - The array of raw data.
+	 */
 	public function toArray()
 	{
 		return $this->raw_data;
+	}
+
+	protected function getUsernameToArray()
+	{
+		return array(
+			'data'			=> &$this->username_data,
+			'appears'		=> &$this->username_appears,
+			'frequency'		=> &$this->username_frequency,
+			'lastseen'		=> &$this->username_lastseen,
+		);
+	}
+
+	protected function getEmailToArray()
+	{
+		return array(
+			'data'			=> &$this->email_data,
+			'appears'		=> &$this->email_appears,
+			'frequency'		=> &$this->email_frequency,
+			'lastseen'		=> &$this->email_lastseen,
+		);
+	}
+
+	protected function getIPToArray()
+	{
+		return array(
+			'data'			=> &$this->ip_data,
+			'appears'		=> &$this->ip_appears,
+			'frequency'		=> &$this->ip_frequency,
+			'lastseen'		=> &$this->ip_lastseen,
+		);
 	}
 }
