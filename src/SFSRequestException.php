5dcfec0da084b9ca929dda8dc4d4eddb2d58c676
Obsidian <stratosphere.programming@gmail.com>
2010-10-17 08:38:57 -0500

    toss in the exception files and a bunch of docs for them

--- src/SFSRequestException.php
+++ src/SFSRequestException.php
@@ -19,16 +19,21 @@
  */
 
 /**
- * SFS Integration - Exception class,
- *      Extension of the default Exception class, used for organizing exceptions.
+ * SFS Integration - Subordinate exception class,
+ *      Extension of the SFS exception class.
  *
  *
  * @package     sfsintegration
  * @author      Damian Bushong
  * @license     MIT License
  * @link        http://github.com/Obsidian1510/SFSIntegration
+ *
+ * @note reserves 110xx error codes
  */
-class SFSException extends Exception
+class SFSRequestException extends SFSException
 {
-	const ERR_WTF = 0;
+	const ERR_NO_REQUEST_DATA = 11000;
+	const ERR_API_RETURN_EMPTY = 11001;
+	const ERR_API_RETURNED_BAD_JSON = 11002;
+	const ERR_API_RETURNED_ERROR = 11003;
 }
