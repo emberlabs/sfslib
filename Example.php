b4fcb5ade6122188372ef1ace8d3522e247853ae
Obsidian <stratosphere.programming@gmail.com>
2010-11-08 16:00:07 -0600

    update the example file with code reflecting how one would report spammers, once SFSReport is done...

--- Example.php
+++ Example.php
@@ -36,8 +36,14 @@ spl_autoload_register('SFS::loader');
 // Instantiate the main StopForumSpam interaction object.
 $sfs = new SFS();
 
+//###############################################################################
 // Example of how to set a bunch of options on the SFS library before you request...
-$sfs->setTimeout(10);
+//###############################################################################
+$sfs->setTimeout(10)->setRequestMethod(SFS::TRX_REQUEST_CURL);
+
+//###############################################################################
+// Example of making a lookup request to StopForumSpam's API
+//###############################################################################
 
 try
 {
@@ -55,3 +61,24 @@ catch(SFSException $e)
 print_r($result['username']['lastseen']); // object of type DateTime
 var_dump($result['email']); // array containing all result data that is available for the email looked up
 echo $result->getIPFrequency(); // integer of how many times the IP was found in the StopForumSpam database
+
+
+
+//###############################################################################
+// Example of how to report a spammer to StopForumSpam...
+//###############################################################################
+
+try
+{
+	// @note $result is an object of type SFSReportResult, and can have its properties accessed as an array, or through its built-in methods.
+	$result = $sfs->newReport()->setUsername('Some username here')->setEmail('someemail@email.tld')->setIP('127.0.0.2')->setAPIKey('abcdef12345678')->send();
+}
+catch(SFSException $e)
+{
+	// error handling goes here, handle errors however you'd want.
+	echo $e->getMessage() . PHP_EOL;
+	exit;
+}
+
+// Should just be the success message.
+echo $result->getMessage();
