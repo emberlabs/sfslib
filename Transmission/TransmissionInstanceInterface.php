d69bdd0f6410dfdeb8aec9cabf41c4332b0eb22a
damianb <damian@emberlabs.org>
2011-10-19 10:13:06 -0500

    current dev

---
+++ Transmission/TransmissionInstanceInterface.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  StopForumSpam integration library
+ *-------------------------------------------------------------------
+ * @package     sfslib
+ * @author      emberlabs.org
+ * @copyright   (c) 2010 - 2011 Damian Bushong
+ * @license     MIT License
+ * @link        https://github.com/emberlabs/sfslib
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace emberlabs\sfslib\Transmission;
+/**
+ * StopForumSpam integration library - Transmission instance interface
+ * 	     Provides a prototype of methods that all transmission instances should provide.
+ *
+ * @package     sfslib
+ * @author      emberlabs.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/emberlabs/sfslib
+ */
+interface TransmissionInstanceInterface
+{
+	public function send(\emberlabs\sfslib\Transmission\TransmissionInstanceInterface $transmission)
+	{
+		// Set the stream timeout, just in case
+		$stream = stream_context_create(array(
+			'http'	=> array(
+				'timeout'	=> Core::getConfig('sfs.timeout'),
+			),
+		));
+
+		$json = @file_get_contents($transmission->buildURL() . '&useragent=' . rawurlencode(Core::getUserAgent()), false, $stream);
+
+		return $transmission->newResponse($json);
+	}
+}
