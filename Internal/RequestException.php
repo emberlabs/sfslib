a82794f86bb92e61e783894df3468d097ff42499
damianb <damian@emberlabs.org>
2011-10-19 12:30:08 -0500

    Another exception

---
+++ Internal/RequestException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ *===================================================================
+ *
+ *  StopForumSpam integration library
+ *-------------------------------------------------------------------
+ * @package     sfslib
+ * @author      emberlabs.org
+ * @copyright   (c) 2010 - 2011 emberlabs.org
+ * @license     MIT License
+ * @link        https://github.com/emberlabs/sfslib
+ *
+ *===================================================================
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.
+ *
+ */
+
+namespace emberlabs\sfslib\Internal;
+
+/**
+ * StopForumSpam integration library - Request Exception class,
+ * 	     used when something goes asplodie regarding the preparation of a request to StopForumSpam.
+ *
+ * @package     sfslib
+ * @author      emberlabs.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/emberlabs/sfslib
+ */
+class RequestException extends InternalException { }
